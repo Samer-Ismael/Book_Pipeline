39b763c3b7bc1b346051d968a1b3331064852ac6
ellen <ellenhalvardsson@gmail.com>
2024-02-27 14:18:11 +0100

    entity author and book

---
+++ src/main/java/com/BookPipeline/BookPipeline/entity/Author.java
@@ -0,0 +1,25 @@
+package com.BookPipeline.BookPipeline.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE)
+    @JsonIgnore
+    private List<Book> books = new ArrayList<>();
+}
