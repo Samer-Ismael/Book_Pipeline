2902aec068ed49922ac6878e708e58921b808370
KoffaRn <lilamoln.koffa@gmail.com>
2024-03-13 18:47:36 +0100

    Annotaded SaveAuthorRequest.java with @Data to get all Getter and Setter methods (setters was not implemented)

--- src/main/java/com/BookPipeline/BookPipeline/model/SaveAuthorRequest.java
+++ src/main/java/com/BookPipeline/BookPipeline/model/SaveAuthorRequest.java
@@ -1,8 +1,14 @@
 package com.BookPipeline.BookPipeline.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaveAuthorRequest {
     private String name;
 }
